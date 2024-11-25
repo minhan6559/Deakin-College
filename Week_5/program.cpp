d8e4cda6d427a47e8bf99ead9b9f8cc78d9d76a3
minhan6559 <minhan6559@gmail.com>
2024-11-25 10:12:23 +0700

    update week 5

---
+++ Week_5/program.cpp
@@ -0,0 +1,25 @@
+#include "splashkit.h"
+
+int main()
+{
+    // Biến - Variable
+    // Kiểu dữ liệu - Data type
+
+    // Số nguyên - Integer
+    int num_people = 1;
+
+    // Số thực - Real number
+    double pi = 3.14;
+
+    // Ký tự - Character
+    char letter = 'a';
+
+    // Chuỗi ký tự - String
+    string name = "DangLy";
+
+    // Boolean - True/False - 1/0
+    bool is_tall = false;
+    bool is_short = true;
+
+    return 0;
+}
